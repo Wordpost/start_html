70104634a2d0d6818f8312e705c80d1f10d4489c
wordpost <wordpost@ya.ru>
2018-03-30 12:14:47 +0400

    added smart grid

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,7 @@ global.$ = {
 	loadPlugin: require('gulp-load-plugins')(),
 	browserSync: require('browser-sync').create(),
 	svgSprite: require('gulp-svg-sprites'),
+	smartgrid: require('smart-grid'),
 	path: {
 		tasks: require('./gulp/config/tasks.js'),
 		build: {
@@ -35,7 +36,32 @@ global.$ = {
 			sprites:  'src/img/sprites/*.*',
 			svg:      'src/img/svg-icon/*.svg',
 			fonts:    'src/fonts/**/*.*'
-		}
+		},
+		gridSettings: {
+			// https://www.npmjs.com/package/smart-grid#why-how-does-it-work
+			outputStyle: 'scss', /* less || scss || sass || styl */
+			columns: 12, /* number of grid columns */
+			offset: '30px', /* gutter width px || % */
+			mobileFirst: true, /* mobileFirst ? 'min-width' : 'max-width' */
+			container: {
+				maxWidth: '1200px', /* max-width оn very large screen */
+				fields: '15px' /* side fields */
+			},
+			breakPoints: {
+				xl: {
+					width: '1230px',
+				},
+				lg: {
+					width: '992px'
+				},
+				md: {
+					width: '768px',
+				},
+				sm: {
+					width: '576px'
+				}
+			}
+		},
 	}
 };
 
@@ -48,3 +74,4 @@ $.gulp.task('default', $.gulp.series(
 	$.gulp.parallel('watch', 'server')
 	)
 );
+$.smartgrid('./src/sass/plug/', $.path.gridSettings);
